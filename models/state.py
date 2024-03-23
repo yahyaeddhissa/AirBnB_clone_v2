e04a39aa80080dd0f45dda29280fd5c90bc90de5
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-03-23 20:55:31 +0000

    state and cities now rely on dbstorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models import storage_type
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+
+    __tablename__ = "states"
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City", backref="state", cascade="all, delete, delete-orphan"
+        )
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id
+            equals the current State.id
+            FileStorage relationship between State and City
+            """
+            from models import storage
+
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
