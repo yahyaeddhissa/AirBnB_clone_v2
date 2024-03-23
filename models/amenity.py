940c35597c85592601bbe563ed239ec91192f080
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-03-23 21:08:40 +0000

    Update the rest of the classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from sqlalchemy import Column, String
+
+from models import storage_type
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """amenity class"""
+
+    __tablename__ = "amenities"
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
